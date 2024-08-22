711f8626b7eac2b4c88736d3451aa668e882d93e
Barmwato <collinskibor@gmail.com>
2024-08-22 17:47:15 +0300

    Codes to user input project

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Get user's name
+name = input("Enter your name:")
+
+# Get user's age and convert it to an integer (as age is a number)
+age = int(input("Enter your age:"))
+
+# Get user's location
+location = input("Enter your location:")
+
+# Print a personalized message using f-strings
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
